45e3cd0bde20584c14ae441ef3a0122ed6f22758
joellaprade <145079600+joellaprade@users.noreply.github.com>
2023-11-20 23:09:49 -0600

    ew

--- app.js
+++ app.js
@@ -87,6 +87,12 @@ app.put('/putEvent/:id', async (req, res) => {
         }, {new: true})
 })
 
+app.delete('/deleteEvent/:id', async (req, res) => {
+    const id = req.params.id;
+    if(await authenticate(req.body.authentication, 'admin'))
+        await Event.findByIdAndDelete(id)
+})
+
 async function authenticate(data, level){
     var mail = data.mail;
     var password = data.password
